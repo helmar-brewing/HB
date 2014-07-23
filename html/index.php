a6734291c1bea599b6e391883ca10fdb9b7401d3
Stev <topshelfsmith@gmail.com>
2014-07-23 10:45:25 -0400

    Merge Test
    Testing branch after merge.

--- html/index.php
+++ html/index.php
@@ -20,10 +20,6 @@ ob_end_flush();
 
 
 
-
-
-
-
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
