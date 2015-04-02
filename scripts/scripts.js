38580caadbbb6b05cca707e78741bd6fb55c29ed
Stev <topshelfsmith@gmail.com>
2015-04-02 14:12:16 -0400

    added a respond.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,6 @@
 // @koala-prepend "jquery-1.11.1.min.js"
 // @koala-prepend "modernizr.js"
+// @koala-prepend "respond.js"
 // @koala-prepend "selectivizr-min.js"
 // @koala-prepend "phnx-modal.js"
 // @koala-prepend "_nav.js"
@@ -11,6 +12,7 @@
 
 // @codekit-prepend "jquery-1.11.1.min.js"
 // @codekit-prepend "modernizr.js"
+// @codekit-prepend "respond.js"
 // @codekit-prepend "selectivizr-min.js"
 // @codekit-prepend "phnx-modal.js"
 // @codekit-prepend "_nav.js"
