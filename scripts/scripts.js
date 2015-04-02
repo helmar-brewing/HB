22848de52ddd38bcf133766b0cfe5b0a32ce1dce
Stev <topshelfsmith@gmail.com>
2015-04-02 14:08:12 -0400

    reconfigured javascript with codekit
    I am getting a JS error in IE8 and am trying to track it down.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,3 +6,14 @@
 // @koala-prepend "_home.js"
 // @koala-prepend "_account.js"
 // @koala-prepend "_subscriptions.js"
+
+
+
+// @codekit-prepend "jquery-1.11.1.min.js"
+// @codekit-prepend "modernizr.js"
+// @codekit-prepend "selectivizr-min.js"
+// @codekit-prepend "phnx-modal.js"
+// @codekit-prepend "_nav.js"
+// @codekit-prepend "_home.js"
+// @codekit-prepend "_account.js"
+// @codekit-prepend "_subscriptions.js"
