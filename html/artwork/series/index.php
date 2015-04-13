6ccec92dfa87f0ed9c79e097c519e70c2ff865f4
radam13 <github@tek13.com>
2015-04-12 21:18:25 -0400

    series code update
    added subscription status to series code

--- html/artwork/series/index.php
+++ html/artwork/series/index.php
@@ -20,10 +20,10 @@ require_once('classes/phnx-user.class.php');
 $currentpage = 'artwork/';
 
 // Card Series Variables
-$seriesID = $_GET['series'];
+$series_id = $_GET['series'];
 //$series_id = 'R319';
 
-$series_sql = $db_main->query("SELECT * FROM series_info WHERE series_id='".$_GET['series']."' LIMIT 1");
+$series_sql = $db_main->query("SELECT * FROM series_info WHERE series_id='".$series_id."' LIMIT 1");
 if($series_sql !== FALSE){
     $series_sql->data_seek(0);
     while($seriesinfo = $series_sql->fetch_object()){
@@ -46,6 +46,10 @@ $user = new phnx_user;
 
 // check user login status
 $user->checklogin(1);
+
+// check user subscription status
+$user->checklogin(1);
+$user->checksub();
 
 ob_end_flush();
 /* <HEAD> */ $head=''; // </HEAD>
@@ -74,58 +78,178 @@ print'
 
  if(isset($user)){
     if( $user->login() == 1 || $user->login() == 2 ){
-		/* do this code if user is logged in, but not paid subscription */
+		/* do this code if user is logged in */
 
-print '
-        <table class="tables">
-          <thead>
-            <tr>
-              <th>Card Number</th>
-              <th>Player</th>
-              <th>Stance / Position</th>
-              <th>Team</th>
-            </tr>
-          </thead>
-          <tbody>
-';
+    $R_cards = $db_main->query("SELECT * FROM cardList WHERE series = '".$series_tag."'");
 
-$i = 0;
-$R_cards = $db_main->query("SELECT * FROM cardList WHERE series = '".$series_tag."'");
-if($R_cards !== FALSE){
-    $R_cards->data_seek(0);
-    while($card = $R_cards->fetch_object()){
-        print'
-            <tr>
-                <td>'.$card->cardnum.'</td>
-                <td>'.$card->player.'</td>
-                <td>'.$card->description.'</td>
-                <td>'.$card->team.'</td>
-        ';
+      // if user does NOT have subscription:
+      if $user->subscription['status'] != 'active' {
+                print '
+                        <table class="tables">
+                          <thead>
+                            <tr>
+                              <th>Card Number</th>
+                              <th>Player</th>
+                              <th>Stance / Position</th>
+                              <th>Team</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                ';
 
-        print'
-                </td>
-            </tr>
-        ';
-        $i++;
-        $updated = $card->updatedate;
-    }
-    $R_cards->free();
-}else{
-    print'
-        <tr><td colspan="4">could not get list of cards</td></tr>
-    ';
-}
-print'
-          </tbody>
-        </table>
-        <p>
-            Card list last updated: '.$updated.'<br/>
-            Number of Records: '.$i.'
-        </p>
+                $i = 0;
+                if($R_cards !== FALSE){
+                    $R_cards->data_seek(0);
+                    while($card = $R_cards->fetch_object()){
+                        print'
+                            <tr>
+                                <td>'.$card->cardnum.'</td>
+                                <td>'.$card->player.'</td>
+                                <td>'.$card->description.'</td>
+                                <td>'.$card->team.'</td>
+                        ';
 
-';
+                        print'
+                                </td>
+                            </tr>
+                        ';
+                        $i++;
+                        $updated = $card->updatedate;
+                    }
+                    $R_cards->free();
+                }else{
+                    print'
+                        <tr><td colspan="4">could not get list of cards</td></tr>
+                    ';
+                }
+                print'
+                          </tbody>
+                        </table>
+                        <p>
+                            Card list last updated: '.$updated.'<br/>
+                            Number of Records: '.$i.'
+                        </p>
+
+                ';
+
+      }else{
+        // do this if the user subscription = active
+
+            print'
+                    <table class="tables">
+                      <thead>
+                        <tr>
+                          <th>Card Number</th>
+                          <th>Player</th>
+                          <th>Stance / Position</th>
+                          <th>Team</th>
+                          <th>Last Sold Date</th>
+                          <th>Max Sell Price</th>
+                          <th>Pictures</th>
+            			        <th>Personal Checklist</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+            ';
+            $i = 0;
+            if($R_cards !== FALSE){
+                $R_cards->data_seek(0);
+                while($card = $R_cards->fetch_object()){
+                    print'
+                        <tr>
+                            <td>'.$card->cardnum.'</td>
+                            <td>'.$card->player.'</td>
+                            <td>'.$card->description.'</td>
+                            <td>'.$card->team.'</td>
+                    ';
+                    if($card->averagesold == 0){
+                        print'
+                            <td></td>
+                            <td></td>
+                        ';
+                    }else{
+                        print'
+                            <td>'.$card->lastsold.'</td>
+                            <td>'.$card->maxSold.'</td>
+                        ';
+                    }
+
+                    // need to add ************ for one row, check if picture exists
+                    print'
+                            <td align="center" class="picCol">
+                    ';
 
 
+                    // define the pictures
+                    $frontpic = '/images/cardPics/'.$card->series.'_'.$card->cardnum.'_Front.jpg';
+                    $frontthumb = '/images/cardPics/thumb/'.$card->series.'_'.$card->cardnum.'_Front_small.jpg';
+                    $backpic  = '/images/cardPics/'.$card->series.'_'.$card->cardnum.'_Back.jpg';
+                    $backthumb  = '/images/cardPics/thumb/'.$card->series.'_'.$card->cardnum.'_Back_small.jpg';
+
+                    //check if either pic exists
+                    if( file_exists($_SERVER['DOCUMENT_ROOT'].$frontpic) || file_exists($_SERVER['DOCUMENT_ROOT'].$backpic) ){
+
+                        // print the front pic if exists
+                        if(file_exists($_SERVER['DOCUMENT_ROOT'].$frontpic)){
+                            print'
+                                <a href="http://www.helmarbrewing.com/'.$frontpic.'" data-lightbox="'.$card->series.'_'.$card->cardnum.'" ><img src="http://www.helmarbrewing.com/'.$frontthumb.'"></a>
+                            ';
+                        }
+
+                        // insert space
+                        if( file_exists($_SERVER['DOCUMENT_ROOT'].$frontpic) && file_exists($_SERVER['DOCUMENT_ROOT'].$backpic) ){
+                            print'&nbsp;&nbsp;';
+                        }
+
+                        // print the back pic if exists
+                        if(file_exists($_SERVER['DOCUMENT_ROOT'].$backpic)){
+                            print'
+                                <a href="http://www.helmarbrewing.com/'.$backpic.'" data-lightbox="'.$card->series.'_'.$card->cardnum.'" ><img src="http://www.helmarbrewing.com/'.$backthumb.'"></a>
+                            ';
+
+                        }
+
+                    // neither pic exists print message instead
+                    }else{
+                        print'
+                                <i>no picture</i>
+                        ';
+                    }
+            		/* end card pic */
+                    print'</td>';
+
+            		/* need to add ajax code , how to add fontawesome icons? */
+            		print '<td><i class="fa fa-user-plus"></i></td>';
+            //		print '<td><img src="'.$protocol.$site.'/img/add_new_icon.gif" alt="'.$card->series.','.$card->cardnum.'"></td>';
+
+
+
+            		/* end row */
+            		print '</tr>';
+
+                    $i++;
+                    $updated = $card->updatedate;
+                }
+                $R_cards->free();
+            }else{
+                print'
+                    <tr><td colspan="7">could not get list of cards</td></tr>
+                ';
+            }
+            print'
+                      </tbody>
+                    </table>
+                    <p>
+                        Card list last updated: '.$updated.'<br/>
+                        Number of Records: '.$i.'
+                    </p>
+
+                </div>
+
+
+            ';
+
+      }
 
 
 
