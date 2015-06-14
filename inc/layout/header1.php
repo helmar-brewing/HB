e40a15b345ea51d0db0f3cb5b303b446ee50192b
Stev <topshelfsmith@gmail.com>
2015-06-14 16:47:47 -0400

    resolve conflicts
    resolved conflicts and uncommented the subscribe image on the artwork
    page

--- inc/layout/header1.php
+++ inc/layout/header1.php
@@ -21,25 +21,6 @@ print'
                 <li class="nav-link"><a href="http://helmarblog.com/" target="_blank">Blog</a></li>
                 <li class="nav-link"><a href="http://stores.ebay.com/Helmar-Brewing-Art-and-History/" target="_blank">eBay Store</a></li>
 ';
-<<<<<<< HEAD
-// if(isset($user)){
-//     if( $user->login() == 1 || $user->login() == 2 ){
-//         print'
-//                 <li class="nav-link"><a href="'.$protocol.$site.'/account/logout">Log out</a></li>
-//         ';
-//     }else{
-//         print'
-//                 <li class="nav-link"><a href="'.$protocol.$site.'/account/login?redir='.$currentpage.'">Log in</a></li>
-//                 <li class="nav-link"><a href="'.$protocol.$site.'/account/register">Register</a></li>
-//         ';
-//     }
-// }else{
-//     print'
-//                 <li class="nav-link"><a href="'.$protocol.$site.'/account/login?redir='.$currentpage.'">Log in</a></li>
-//                 <li class="nav-link"><a href="'.$protocol.$site.'/account/register">Register</a></li>
-//     ';
-// }
-=======
 if(isset($user)){
     if( $user->login() == 1 || $user->login() == 2 ){
         print'
@@ -58,7 +39,6 @@ if(isset($user)){
                 <li class="nav-link"><a href="'.$protocol.$site.'/account/register">Register</a></li>
     ';
 }
->>>>>>> version-2
 print'
             </ul>
         </div>
