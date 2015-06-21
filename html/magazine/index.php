1a5fd98e1683b4daecb2ce8569821e316fe7669a
Stev <topshelfsmith@gmail.com>
2015-06-21 08:57:27 -0400

    subscription magazine serve

---
+++ html/magazine/index.php
@@ -0,0 +1,92 @@
+<?php
+ob_start();
+
+/* ROOT SETTINGS */ require($_SERVER['DOCUMENT_ROOT'].'/root_settings.php');
+
+/* FORCE HTTPS FOR THIS PAGE */ forcehttps();
+
+/* WHICH DATABASES DO WE NEED */
+$db2use = array(
+	'db_auth' 	=> TRUE,
+	'db_main'	=> TRUE
+);
+
+/* GET KEYS TO SITE */ require($path_to_keys);
+
+/* LOAD FUNC-CLASS-LIB */
+require_once('classes/phnx-user.class.php');
+require_once('libraries/stripe/init.php');
+\Stripe\Stripe::setApiKey($apikey['stripe']['secret']);
+
+/* PAGE VARIABLES */
+$currentpage = '';
+class AuthException extends Exception{}
+class SubException extends Exception{}
+
+$user = new phnx_user;
+$user->checklogin(1);
+$user->checksub();
+
+$db_auth->close();
+$db_main->close();
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // remove this if you already use exceptions for all mysqli queries
+try{
+
+    $file = 'content/'.$_GET['file'];
+
+	if($user->login() !== 1){
+        throw new AuthException('');
+    }
+
+	switch($user->subscription[status]){
+		case 'error':
+            throw new SubException('');
+			break;
+		case 'none':
+            print 'no sub'; // redirect to sub page with error
+			break;
+		case 'active':
+			if($user->subscription['digital'] === TRUE){
+                if(file_exists($file)){
+                    header('Content-Type: ' . mime_content_type($file));
+                    header('Content-Length: ' . filesize($file));
+                    readfile($file);
+                }else{
+                    http_response_code(404);
+                }
+            }else{
+                print 'no paper sub'; // redirect to sub page with error
+            }
+			break;
+		default:
+            throw new SubException('');
+			break;
+	}
+
+}catch(SubException $e){
+    http_response_code(500);
+}catch(AuthException $e){
+    header('Location: '.$protocol.$site.'/account/login/?redir='.$currentpage,TRUE,303);
+    ob_end_flush();
+    exit;
+}catch(\Stripe\Error\Card $e) {
+    http_response_code(503);
+}catch(\Stripe\Error\InvalidRequest $e) {
+    http_response_code(503);
+}catch(\Stripe\Error\Authentication $e) {
+    http_response_code(503);
+}catch(\Stripe\Error\ApiConnection $e) {
+    http_response_code(503);
+}catch(\Stripe\Error\Base $e) {
+    http_response_code(503);
+}catch(mysqli_sql_exception $e){
+    http_response_code(503);
+}catch(Exception $e){
+    http_response_code(500);
+}
+mysqli_report(MYSQLI_REPORT_ERROR ^ MYSQLI_REPORT_STRICT); // remove this if you already use exceptions for all mysqli queries
+
+ob_end_flush();
+
+?>
