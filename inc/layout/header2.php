05c93092f7f043420932ccb3d381321f4257c443
Stev <topshelfsmith@gmail.com>
2015-06-24 07:35:28 -0400

    switched logo on inner pages

--- inc/layout/header2.php
+++ inc/layout/header2.php
@@ -2,7 +2,7 @@
 print'
     <div class="header2">
         <a href="'.$protocol.$site.'/" class="logo2">
-            <img src="/img/helmar_logo_centered.png" />
+            <img src="/img/helmar_logo_framed.jpg" />
         </a>
     </div>
 ';
