4f8a96877c0e83bfc8b987a01ccdac81e28557fb
radam13 <github@tek13.com>
2015-08-18 14:32:45 -0400

    test
    test

--- html/index.php
+++ html/index.php
@@ -27,7 +27,7 @@ $user->checklogin(1);
 
 ob_end_flush();
 /* <HEAD> */ $head=''; // </HEAD>
-/* PAGE TITLE */ $title='Helmar Brewing Co';
+/* PAGE TITLE */ $title='Helmar Brewing Co2';
 /* HEADER */ require('layout/header0.php');
 
 print'
