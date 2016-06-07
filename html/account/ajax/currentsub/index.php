e4984a61f04c70eb41fca3767b2eccf2828b55b8
Stev <topshelfsmith@gmail.com>
2016-06-07 10:45:02 -0400

    updated current subscription script

--- html/account/ajax/currentsub/index.php
+++ html/account/ajax/currentsub/index.php
@@ -49,34 +49,6 @@ try{
 
 
 
-	function subName($sub){
-		switch($sub){
-			case 'sub-digital':
-				return 'Digital Magazine';
-				break;
-			case 'sub-paper':
-				return 'Paper Magazine';
-				break;
-			case 'sub-digital+paper':
-				return 'Digital + Paper Magazine';
-				break;
-			default: throw new Exception('Cannot get subscription name, invalid plan type.');
-		}
-	}
-	function subAction($sub){
-		switch($sub){
-			case 'sub-digital':
-				return 'digital';
-				break;
-			case 'sub-paper':
-				return 'paper';
-				break;
-			case 'sub-digital+paper':
-				return 'digitalpaper';
-				break;
-			default: throw new Exception('Cannot get subscription action, invalid plan type.');
-		}
-	}
 	switch($user->subscription[status]){
 		case 'error':
 			$html = '<p>There was an error determining you subscription status.</p>';
@@ -85,19 +57,38 @@ try{
 			$html = '<p>You do not have an active subscription.</p>';
 			break;
 		case 'active':
-			$html = '<p>You are currently subscribed to <span>'.subName($user->subscription['plan_type']).'</span></p>';
+			$payment = $user->subscription['next_payment'] / 100;
+			$html = '<p>You are currently subscribed to <span>Digital + Paper Magazine</span></p>';
 			if($user->subscription['cancel_at_period_end'] === true){
 				$html .= '<p><i class="fa fa-ban"></i> Auto re-new is turned off. Your subscription will be canceled on <span>'.date('M j Y', $user->subscription['current_period_end']).'</span>.</p>';
 				$html .= '<button onclick="sub(\''.subAction($user->subscription['plan_type']).'\')">Resume Subscription</button>';
 			}else{
-				if($user->subscription['plan_type'] === $user->subscription['next_plan']){
-					$html .= '<p><i class="fa fa-refresh"></i> Your subscription will renew on <span>'.date('M j Y', $user->subscription['current_period_end']).'</span>.</p>';
-					$html .= '<p>Next Payment: $'.substr($user->subscription['next_payment'],0,-2).'.'.substr($user->subscription['next_payment'],-2).' on '.date('M j Y', $user->subscription['current_period_end']).'.</p>';
-				}else{
-					$html .= '<p><i class="fa fa-level-down"></i> You have elected to change your subscription to <span>'.subName($user->subscription['next_plan']).'</span> effective on <span>'.date('M j Y', $user->subscription['current_period_end']).'</span>.</p>';
-					$html .= '<p>Next Payment: $'.substr($user->subscription['next_payment'],0,-2).'.'.substr($user->subscription['next_payment'],-2).' on '.date('M j Y', $user->subscription['current_period_end']).'.</p>';
-				}
+				$html .= '<p><i class="fa fa-refresh"></i> Your subscription will renew on <span>'.date('M j Y', $user->subscription['current_period_end']).'</span>.</p>';
+				$html .= '<p>Next Payment: $'.$payment.' on '.date('M j Y', $user->subscription['current_period_end']).'.</p>';
 			}
+			break;
+		case 'trialing':
+			$payment = $user->subscription['next_payment'] / 100;
+			$html = '<p>You are currently subscribed to <span>Digital + Paper Magazine</span></p>';
+			if($user->subscription['cancel_at_period_end'] === true){
+				$html .= '<p><i class="fa fa-ban"></i> Auto re-new is turned off. Your subscription will be canceled on <span>'.date('M j Y', $user->subscription['current_period_end']).'</span>.</p>';
+				$html .= '<button onclick="sub(\''.subAction($user->subscription['plan_type']).'\')">Resume Subscription</button>';
+			}else{
+				$html .= '<p><i class="fa fa-refresh"></i> Your subscription will renew on <span>'.date('M j Y', $user->subscription['current_period_end']).'</span>.</p>';
+				$html .= '<p>Next Payment: $'.$payment.' on '.date('M j Y', $user->subscription['current_period_end']).'.</p>';
+			}
+			break;
+		case 'past_due':
+			$payment = $user->subscription['next_payment'] / 100;
+			$html .= '<p>You are currently subscribed to <span>Digital + Paper Magazine</span></p>';
+			$html .= 'Your subscription is past due.';
+			$html .= '<p>Next Payment: $'.$payment.' on '.date('M j Y', $user->subscription['current_period_end']).'.</p>';
+			break;
+		case 'unpaid':
+			$html = 'Your subscription is unpaid.';
+			break;
+		case 'canceled':
+			$html = '<p>You do not have an active subscription.</p>';
 			break;
 		default:
 			$html = '<p>There was an error determining you subscription status.</p>';
